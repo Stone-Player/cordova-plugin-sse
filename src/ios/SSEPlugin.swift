4b85b1a30c876e2de3aae7f2081564e38a48be92
André Grillo <andregrillo@gmail.com>
2023-08-01 11:51:49 +0100

    Fixed local callback variable

--- src/ios/SSEPlugin.swift
+++ src/ios/SSEPlugin.swift
@@ -57,7 +57,7 @@ import AlamofireEventSource
     func stopEventSource(command: CDVInvokedUrlCommand) {
         self.eventSourceRequest?.cancel()
         self.eventSourceRequest = nil
-        sendPluginResult(callbackId: self.callbackId, status: CDVCommandStatus_OK, message: "Event Source Stopped")
+        sendPluginResult(callbackId: command.callbackId, status: CDVCommandStatus_OK, message: "Event Source Stopped")
     }
     
     func sendPluginResult(callbackId: String, status: CDVCommandStatus, message: String, keepCallback: Bool = false) {
